0558ec29ff07d332bb4991247d80f52cb1e03576
cubi4 <abrau2012@web.de>
2024-11-26 13:12:11 -0300

    added: variables in tablescript with json data
    adjusted: json format

--- src/table-script.tsx
+++ src/table-script.tsx
@@ -1,8 +1,4 @@
-import {
-  MaterialReactTable,
-  useMaterialReactTable,
-  type MRT_ColumnDef,
-} from "material-react-table";
+import { MaterialReactTable, useMaterialReactTable, type MRT_ColumnDef } from "material-react-table";
 import { useMemo } from "react";
 import sourceData from "./source-data.json";
 import type { SourceDataType, TableDataType } from "./types";
@@ -20,65 +16,70 @@ import type { SourceDataType, TableDataType } from "./types";
  * @prop {number} netEarningsPrevMonth - The net earnings for the previous month.
  */
 
-const tableData: TableDataType[] = (
-  sourceData as unknown as SourceDataType[]
-).map((dataRow, index) => {
-  const person = `${dataRow?.employees?.firstname} - ...`;
+const tableData: TableDataType[] = (sourceData as unknown as SourceDataType[]).map((dataRow, index) => {
+    const person = `${dataRow?.employees?.firstname} ${dataRow?.employees?.lastname}`;
+    const LastTwelveMonths = dataRow?.employees?.workforceUtilisation?.utilisationRateLastTwelveMonths;
+    const Y2D = dataRow?.employees?.workforceUtilisation?.utilisationRateYearToDate;
+    const august = dataRow?.employees?.workforceUtilisation?.lastThreeMonthsIndividually?.filter((e) => e.month === "August")[0]?.utilisationRate;
+    const june = dataRow?.employees?.workforceUtilisation?.lastThreeMonthsIndividually?.filter((e) => e.month === "June")[0]?.utilisationRate;
+    const july = dataRow?.employees?.workforceUtilisation?.lastThreeMonthsIndividually?.filter((e) => e.month === "July")[0]?.utilisationRate;
 
-  const row: TableDataType = {
-    person: `${person}`,
-    past12Months: `past12Months ${index} placeholder`,
-    y2d: `y2d ${index} placeholder`,
-    may: `may ${index} placeholder`,
-    june: `june ${index} placeholder`,
-    july: `july ${index} placeholder`,
-    netEarningsPrevMonth: `netEarningsPrevMonth ${index} placeholder`,
-  };
+    // const netEarningsPrevMonth = dataRow?.employees?.workforceUtilisation?.netEarningsPrevMonth;
 
-  return row;
+    const row: TableDataType = {
+        person: `${person}`,
+        past12Months: `${LastTwelveMonths}%`,
+        y2d: `${Y2D}%`,
+        may: `${august}%`,
+        june: `${june}%`,
+        july: `${july}%`,
+        netEarningsPrevMonth: `netEarningsPrevMonth ${index} placeholder`,
+    };
+
+    return row;
 });
 
 const Example = () => {
-  const columns = useMemo<MRT_ColumnDef<TableDataType>[]>(
-    () => [
-      {
-        accessorKey: "person",
-        header: "Person",
-      },
-      {
-        accessorKey: "past12Months",
-        header: "Past 12 Months",
-      },
-      {
-        accessorKey: "y2d",
-        header: "Y2D",
-      },
-      {
-        accessorKey: "may",
-        header: "May",
-      },
-      {
-        accessorKey: "june",
-        header: "June",
-      },
-      {
-        accessorKey: "july",
-        header: "July",
-      },
-      {
-        accessorKey: "netEarningsPrevMonth",
-        header: "Net Earnings Prev Month",
-      },
-    ],
-    []
-  );
+    const columns = useMemo<MRT_ColumnDef<TableDataType>[]>(
+        () => [
+            {
+                accessorKey: "person",
+                header: "Person",
+            },
+            {
+                accessorKey: "past12Months",
+                header: "Past 12 Months",
+            },
+            {
+                accessorKey: "y2d",
+                header: "Y2D",
+            },
+            {
+                accessorKey: "may",
+                header: "May",
+            },
+            {
+                accessorKey: "june",
+                header: "June",
+            },
+            {
+                accessorKey: "july",
+                header: "July",
+            },
+            {
+                accessorKey: "netEarningsPrevMonth",
+                header: "Net Earnings Prev Month",
+            },
+        ],
+        []
+    );
 
-  const table = useMaterialReactTable({
-    columns,
-    data: tableData,
-  });
+    const table = useMaterialReactTable({
+        columns,
+        data: tableData,
+    });
 
-  return <MaterialReactTable table={table} />;
+    return <MaterialReactTable table={table} />;
 };
 
 export default Example;
